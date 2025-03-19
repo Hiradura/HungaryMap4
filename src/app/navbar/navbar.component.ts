2fd60b7f2386984591c80ae4f7f44552ba01c9fd
The Matrix <146129505+Valaki2004@users.noreply.github.com>
2025-03-19 16:09:03 +0100

    Update navbar.component.ts

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { AuthService } from '../auth.service';
 import { Router } from '@angular/router';
 import { SearchService } from '../search.service';
+declare var bootstrap: any;
 
 @Component({
   selector: 'app-navbar',
@@ -16,6 +17,7 @@ export class NavbarComponent {
   admin: any = null 
 
   constructor(private authService: AuthService, public router: Router, private search: SearchService) {}
+  isDropdownOpen = false;
 
   ngOnInit() {
     this.authService.getCurrentUser().subscribe(user => {
@@ -23,10 +25,10 @@ export class NavbarComponent {
     });
     this.authService.getIsAdmin().subscribe(admin => {
       this.admin = admin
-
-    });
-  }
-
+    })}
+    toggleDropdown(state: boolean) {
+      this.isDropdownOpen = state;
+    }
   logout() {
     this.authService.logout();
     this.router.navigate(['/login']);
